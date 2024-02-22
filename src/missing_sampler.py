39fe23f684a796e2c2b96103fb0889f8111e4e06
leostre <strelllleo@mail.ru>
2024-02-22 23:50:45 +0300

    reconcept of misssampler

--- src/missing_sampler.py
+++ src/missing_sampler.py
@@ -1,35 +1,52 @@
 import pandas as pd
 import numpy as np
+
+
 class BaseMissingSampler:
-    def __init__(self):
+    def __init__(self, data, droprate, target_cols=None, random_state=None, **read_kw):
+        assert 0 <= droprate <= 1, 'Droprate is within [0, 1] range!'
         self.path = None
-        self._data = None
-        self.mask = None
-        # self.random_state = random_state
-        self.target_cols = []
+        self._mask = None
+        self.rg = np.random.Generator(np.random.PCG64(random_state))
         self.len = 0
         self.done = False
+        self.droprate = droprate
+        self._data: pd.DataFrame = None
+        self._scan_data(data, **read_kw)
+        if target_cols:
+            self.target_cols = target_cols
+        else:
+            self.target_cols = self._data.columns
 
     def __len__(self):
         return self.len
 
-    def scan_data(self, data, target_cols=None):
-        if target_cols:
-            self.target_cols = target_cols
+    @property
+    def mask(self):
+        return self._mask
+
+    def _scan_data(self, data, **read_kw):
         if isinstance(data, str):
             if data.endswith('.csv'):
-                self._data = pd.read_csv(data)
+                self._data = pd.read_csv(data, **read_kw)
+            elif data.endswith('.parquet'):
+                self._data = pd.read_parquet(data, **read_kw)
             else:
-                raise NotImplementedError
-        else:
+                raise NotImplementedError('Only .csv and .parquet are supported')
+            self.path = data
+        elif isinstance(data, pd.DataFrame):
             self._data = data
+        else:
+            raise ValueError('Only pandas.DataFrame objects and 2D-tables in .csv/.parquet are supported')
+        self.len = self._data.shape[0]
 
-        if hasattr(self._data, 'shape'):
-            self.len = self._data.shape[0]
+    def _generate_mask(self):
+        raise NotImplementedError
 
-    def drop(self, droprate, inplace=False):
-        assert 0 <= droprate <= 1, 'Droprate is within [0, 1] range!'
-        self.done = True
+    def drop(self, inplace=False):
+        self._generate_mask()
+        self._data.loc[:, self.target_cols] = self._data[self.target_cols].where(self._mask, np.nan)
+        return self
 
     def save(self, path):
         if path.endswith('.csv') and isinstance(self._data, pd.DataFrame):
@@ -40,30 +57,24 @@ class BaseMissingSampler:
     @property
     def droppedn(self):
         assert self.done, 'The dropout has\'nt been done yet!'
-        length = len(self.mask)
-        if length > 1:
-            return {col: self.mask[col].sum() for col in self.target_cols}
-        elif length:
-            return self.mask.values()[0]
-        else:
-            return 0
-
-    @property
-    def masks(self):
-        return self.mask
+        return pd.Series(index=self.target_cols, data=self._mask.sum(0), name='Nan number')
 
 
 class UniformMissing(BaseMissingSampler):
-    def drop(self, droprate, inplace=False):
-        super().drop(droprate)
-        for col in self.target_cols:
-            self.mask[col] = (np.random.uniform(0, 1, size=(self.len,)) < droprate)
-        if inplace:
-            self.loc[self.mask[col], self.target_cols] = np.nan
-        else:
-            res = self._data.copy()
-            res.loc[self.mask[col], self.target_cols] = np.nan
-            return res
+    def _generate_mask(self):
+        self._mask = self.rg.random((self._data.shape[0], len(self.target_cols))) < self.droprate
+        self.done = True
+        return self._mask
+
+
+if __name__ == '__main__':
+    um = UniformMissing('../data/refined/wide/blood_chemistry_17.csv', .3,
+                        target_cols=['Potassium',
+                                     'Sodium', 'Creatinine', 'Chloride', 'Urea Nitrogen', 'Bicarbonate',
+                                     'Anion Gap', 'Glucose', 'Magnesium', 'Calcium, Total', 'Phosphate']
+                        )
+    um.drop()
+    print(um.droppedn)
 
 
 
