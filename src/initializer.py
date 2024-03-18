115b7452481a2ede364057edf9682e9706db51be
leostre <strelllleo@mail.ru>
2024-03-18 13:39:33 +0300

    some experiments in libs.ipynb

--- src/initializer.py
+++ src/initializer.py
@@ -1,92 +1,99 @@
 import torch
 from torch_geometric.utils import (
     to_torch_coo_tensor, from_scipy_sparse_matrix, erdos_renyi_graph,
-    barabasi_albert_graph
+    barabasi_albert_graph, 
 )
+from itertools import product
+from misc import discretize, wrong_arguments_display, NotFittedError, get_groups
+from numpy import array, arange, unique
 from torch_geometric.data import Data
 from sklearn.neighbors import kneighbors_graph, radius_neighbors_graph
-# from utils import wrong_arguments_display
-
-# def tab2graph(path, init_method, **init_method_kwargs):
-#     tabular = pd.read_csv(path)
-#     adj = init_method
     
-def wrong_arguments_display(func):
-    def wrap(self, *args, **kwargs):
-        try:
-            res = func(self, *args, **kwargs)
-            return res
-        except TypeError as err:
-            err.args = err.args[0] + f'''
-            Wrong arguments were passed to {func.__name__} of {str(type(self)).split('.')[-1][:-2]}
-            Read the docs: 
-            {self._doc()}''',
-            raise 
-        finally:
-            return res
-    return wrap
-
 
 class BaseInitializer:
-    def __init__(self, data, device='cpu', **init_kwargs):
-        self.data = data
+    __kwargs = frozenset()
+
+    def __init__(self, *, device='cpu', **init_kwargs):
+        if self._check_init_kwargs(init_kwargs):
+            self.init_kwargs = init_kwargs
+        self._fitted = False
+        self.data = None
         self.init_kwargs = init_kwargs
         self.G = None
         self.device = device
-        self.weights = None
-        self.adjlist = None
-        self._gen()
+        self._weights = None
+        self._adjlist = None
 
     @wrong_arguments_display
-    def _gen(self):
-        raise NotImplementedError('It\'s a base class!')
+    def _check_init_kwargs(self, init_kwargs):
+        for val in init_kwargs:
+            if val not in self.__kwargs:
+                raise TypeError
+        return True
+
+    def __call__(self, data):
+        self.data = data
+        self._fitted = True
+    
+    def reset(self):
+        self.__init__(self.device, **self.init_kwargs)
 
     def _doc(self):
         return self.__doc__
 
     @property
     def adjacency_matrix(self,):
-        return to_torch_coo_tensor(self.adjlist, self.weights).to(self.device)
+        if not self._fitted:
+            raise NotFittedError(type(self))
+        return to_torch_coo_tensor(self._adjlist, self._weights).to(self.device)
     
     @property
     def edgelist(self):
-        return self.adjlist.to(self.device)
+        if not self._fitted:
+            raise NotFittedError(type(self))
+        return self._adjlist.to(self.device)
     
     @property
     def graph(self):
-        if not self.weights:
-            self.weights = torch.ones((len(self.adjlist),))
+        if not self._fitted:
+            raise NotFittedError(type(self))
+        if self._weights is None:
+            self._weights = torch.ones((len(self._adjlist),))
+
         return Data(
             x=torch.Tensor(self.data, device=self.device),
-            edge_index=self.adjlist.to(self.device),
-            edge_attr=self.weights.to(self.device)
+            edge_index=self._adjlist.to(self.device),
+            edge_attr=self._weights.to(self.device)
         )
-
-
-class SelfLoop(BaseInitializer):
-    def _gen(self):
-        pass
 
 
 class KNNInit(BaseInitializer):
     """
     n_neighbors: int
     mode: ['connectivity', 'distance']
+    metric: str = 'minkowski'| distance metric
+    p: int | p-degree in minkowski metric
     """
-    @wrong_arguments_display
-    def _gen(self):
+    __kwargs = frozenset(['n_neighbors', 'mode', 'metric', 'p'])
+
+    def __call__(self, data):
+        super().__call__(data)
         spadj = kneighbors_graph(self.data, **self.init_kwargs)
-        self.adjlist, self.weights = from_scipy_sparse_matrix(spadj)
+        self._adjlist, self._weights = from_scipy_sparse_matrix(spadj)
+        return self
       
         
 class EpsilonRadiusInit(BaseInitializer):
     """
-    eps: float - radius of hyper sphere
+    radius: float - radius of hyper sphere
     """
-    @wrong_arguments_display
-    def _gen(self):
+    __kwargs = frozenset(['radius', 'mode', 'metric', 'p'])
+
+    def __call__(self, data):
+        super().__call__(data)
         spadj = radius_neighbors_graph(self.data, **self.init_kwargs)
-        self.adjlist, self.weights = from_scipy_sparse_matrix(spadj)
+        self._adjlist, self._weights = from_scipy_sparse_matrix(spadj)
+        return self
         
 
 class RandomInit(BaseInitializer):
@@ -94,30 +101,53 @@ class RandomInit(BaseInitializer):
     random_graph: ["erdos_renyi", "barabasi_albert"] | Type of random graph 
     edge_prob: float in [0, 1] | Edge probability (approx. graph density)
     """
-    @wrong_arguments_display
-    def _gen(self):
+    def __call__(self, data):
+        super().__call__(data)
         type_ = self.init_kwargs.pop('random_graph', 'erdos_renyi')
         if type_ == 'erdos_renyi':
-            self.adjlist = erdos_renyi_graph(
+            self._adjlist = erdos_renyi_graph(
                 self.data.shape[0], **self.init_kwargs
             ).to(self.device)
         elif type_ == 'barabasi_albert':
             prob = self.init_kwargs.pop('edge_prob', 0)
-            n = self.data.shape[0]
+            n = data.shape[0]
             self.init_kwargs['num_edges'] = int(n * prob)
-            self.adjlist = barabasi_albert_graph(
+            self._adjlist = barabasi_albert_graph(
                 self.data.shape[0], **self.init_kwargs
             ).to(self.device)
         else:
             raise ValueError('random_graph should be in ["erdos_renyi", "barabasi_albert"]')
+        return self
+    
+class FeatureCliqueInit(BaseInitializer):
+    __kwargs = frozenset(['column_indices', 'division_factor', 'discretize_func', 'cols2disc'])
+
+    def __call__(self, data):
+        super().__call__(data)
+        col_ind = self.init_kwargs['column_indices']
+        df = self.init_kwargs['division_factor']
+        discretize = self.init_kwargs['discretize_func']
+        cols2disc = self.init_kwargs['cols2disc']
+        edgelist = []
+        for indices in get_groups(data, col_ind, discretize, cols2disc, df=df):
+            edgelist.extend(x for x in product(indices, indices))
+        self._adjlist = torch.tensor(edgelist).t().to(self.device)
+        self._weights = torch.ones((len(edgelist),)).to(self.device)
+        return self
 
 
 if __name__ == '__main__':
     print('initializer.py')
     from pandas import read_csv
     data = read_csv(r'C:\Users\user\labevent\data\refined\wide\blood_chemistry_17.csv', 
-                    index_col=(0, 1), usecols=(7, 8, 9)).astype('float32').values
-    G = RandomInit(data, random_graph='erdos_renyi', edge_prob=.5)
+                    index_col=(0, 1),
+                    #   usecols=(7, 8, 9)
+                    ).astype('float32')
+    colinds = [1]
+    print(data.columns)
+    data = data.values
+
+    G = FeatureCliqueInit(division_factor=5, column_indices=colinds, discretize_func=discretize)(data)
     print(G.edgelist)
     print(G.adjacency_matrix)
     print(G.graph)
--- src/missing_sampler.py
+++ src/missing_sampler.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from torch import tensor
 
 class BaseMissingSampler:
     def __init__(self, data, droprate, target_cols=None, random_state=None, **read_kw):
@@ -9,7 +9,7 @@ class BaseMissingSampler:
         self._mask = None
         self.rg = np.random.Generator(np.random.PCG64(random_state))
         self.len = 0
-        self.done = False
+        self._done = False
         self.droprate = droprate
         self._data: pd.DataFrame = None
         self._scan_data(data, **read_kw)
@@ -39,15 +39,22 @@ class BaseMissingSampler:
         else:
             raise ValueError('Only pandas.DataFrame objects and 2D-tables in .csv/.parquet are supported')
         self.len = self._data.shape[0]
+    
+    # def gen_mask(self):
+    #     self._generate_mask()
 
-    def _generate_mask(self):
-        raise NotImplementedError
-
-    def drop(self, add_indicator=False):
-        self._generate_mask()
-        self._data.loc[:, self.target_cols] = self._data[self.target_cols].where(self._mask, np.nan)
+    def drop(self, fill_value=np.nan, *, add_indicator=False, inplace=False):
+        if not self._done:
+            self.generate_mask()
+        X = self._data.copy() if not inplace else self._data
+        X.loc[:, self.target_cols] = X[self.target_cols].where(~self._mask, fill_value)
         if add_indicator:
-            self._data.loc[:, 'how_many_missing'] = self._mask.isna().sum(1)
+            if fill_value == np.nan:
+                X.loc[:, 'how_many_missing'] = self._mask.isna().sum(1)
+            else:
+                X.loc[:, 'how_many_missing'] = (self._mask == fill_value).sum(1)
+        if not inplace:
+            return X
         return self
 
     def save(self, path):
@@ -55,28 +62,54 @@ class BaseMissingSampler:
             self._data.to_csv(path)
         else:
             raise NotImplementedError
+        return self
+    
+    @property
+    def done(self):
+        return self._done
 
     @property
     def droppedn(self):
-        assert self.done, 'The dropout has\'nt been done yet!'
+        assert self._done, 'The dropout has\'nt been done yet!'
         return pd.Series(index=self.target_cols, data=self._mask.sum(0), name='Nan number')
+    
+    def to_tensor(self, *, full=True, new=True):
+        if new or not self._done:
+            self._gen()
+        assert self._done, 'The dropout has\'nt been done yet!'
+        if not full:
+            return tensor(self._mask, dtype=bool)
+        full_mask = np.zeros((self._data.shape))
+        for i, col in enumerate(self._data.columns):
+            if col in self.target_cols:
+                full_mask[:, i] = self._mask[:, self.target_cols.index(col)]
+        return tensor(full_mask.astype(bool))
 
-
-class UniformMissing(BaseMissingSampler):
-    def _generate_mask(self):
-        self._mask = self.rg.random((self._data.shape[0], len(self.target_cols))) < self.droprate
-        self.done = True
+    def generate_mask(self, new=True):
+        if new or not self._done:
+            self._gen()
         return self._mask
 
 
+class UniformMissing(BaseMissingSampler):
+    def _gen(self):
+        self._mask = self.rg.random((self._data.shape[0], len(self.target_cols))) < self.droprate
+        self._done = True
+
+
+
+
 if __name__ == '__main__':
+    import os
+    # os.chdir('src')
+    print('missing_sampler.py')
     um = UniformMissing('../data/refined/wide/blood_chemistry_17.csv', .3,
                         target_cols=['Potassium',
                                      'Sodium', 'Creatinine', 'Chloride', 'Urea Nitrogen', 'Bicarbonate',
                                      'Anion Gap', 'Glucose', 'Magnesium', 'Calcium, Total', 'Phosphate']
                         )
-    um.drop()
-    print(um.droppedn)
+    um.generate_mask()
+    print(um.to_tensor())
 
 
 
